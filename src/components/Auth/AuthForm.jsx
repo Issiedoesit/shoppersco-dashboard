0a5abf7c353982476fa12ca16a7e3c91f1df3614
Isioma Ekwemuka <Isyekwe@gmail.com>
2023-01-29 15:54:54 +0100

    center show password icon

--- src/components/Auth/AuthForm.jsx
+++ src/components/Auth/AuthForm.jsx
@@ -42,18 +42,16 @@ const AuthForm = ({ handleSubmit, authHeaderText, authPageType }) => {
             </label>
 
            {authPageType === 'login' && 
-                <label htmlFor={`${authPageType === 'signup' ? 'signUpPassword' : 'signInPassword'}`} className='bg-brandGray28x flex flex-col focus-within:outline focus-within:outline-1 focus-within:outline-brandGreen7x px-10 py-3 rounded-thirty'>
+                <label htmlFor={`${authPageType === 'signup' ? 'signUpPassword' : 'signInPassword'}`} className='relative bg-brandGray28x flex flex-col focus-within:outline focus-within:outline-1 focus-within:outline-brandGreen7x px-10 py-3 rounded-thirty'>
                     <p  className='text-brandGray31x'>Your password</p>
-                    <div className='relative'>
-                        <input type={`${showPassword ? 'text' : 'password'}`} name="" id={`${authPageType === 'signup' ? 'signUpPassword' : 'signInPassword'}`} placeholder='' className='bg-transparent focus:outline-none w-full pr-8 authPassword'/>
-                        <button type='button' onClick={togglePassword} className='absolute right-0 top-fiftyPercent -translate-y-fiftyPercent group'>
+                    <input type={`${showPassword ? 'text' : 'password'}`} name="" id={`${authPageType === 'signup' ? 'signUpPassword' : 'signInPassword'}`} placeholder='' className='bg-transparent focus:outline-none w-full pr-8 authPassword'/>
+                    <button type='button' onClick={togglePassword} className='absolute right-8 top-fiftyPercent -translate-y-fiftyPercent group'>
                             <svg className='group' width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path className='group-hover:stroke-brandGreen7x' opacity="0.4" d="M15.58 12C15.58 13.98 13.98 15.58 12 15.58C10.02 15.58 8.41998 13.98 8.41998 12C8.41998 10.02 10.02 8.42 12 8.42C13.98 8.42 15.58 10.02 15.58 12Z" stroke="#797979" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                             <path className='group-hover:stroke-brandGreen7x' d="M12 20.27C15.53 20.27 18.82 18.19 21.11 14.59C22.01 13.18 22.01 10.81 21.11 9.4C18.82 5.8 15.53 3.72 12 3.72C8.47003 3.72 5.18003 5.8 2.89003 9.4C1.99003 10.81 1.99003 13.18 2.89003 14.59C5.18003 18.19 8.47003 20.27 12 20.27Z" stroke="#797979" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                             </svg>
                             <div className={`bg-brandGray31x group-hover:bg-brandGreen7x h-6 w-0.5 -rotate-45 ${showPassword || 'hidden'} absolute right-3 top-fiftyPercent -translate-y-fiftyPercent translate-x-fiftyPercent`}></div>
-                        </button>
-                    </div>
+                    </button>
                 </label>
             }
 
