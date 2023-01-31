342699a558063dcb6ce4d79acd271f56d8bba38c
Isioma Ekwemuka <Isyekwe@gmail.com>
2023-01-31 13:31:34 +0100

    search in insight pages positioning

--- src/components/BrandManager/BrandInsight/InsightBrand.jsx
+++ src/components/BrandManager/BrandInsight/InsightBrand.jsx
@@ -32,9 +32,9 @@ const BrandInsight = () => {
   
   return (
     <div className='col-span-3 bg-white rounded-ten p-7'>
-      <div className='flex flex-row pb-10 justify-between items-center gap-5'>
+      <div className='flex flex-col sm:flex-row pb-10 justify-between sm:items-center gap-5'>
             <h4 className='font-avenirHeavy'>Brand Insight</h4>
-            <label htmlFor="brandInsightSearch" className='rounded-ten py-2.5 border-0.5 flex flex-row items-center gap-2 pl-2 border-brandGray4x w-sixtyPercent max-w-md'>
+            <label htmlFor="brandInsightSearch" className='rounded-ten py-2.5 border-0.5 flex flex-row items-center gap-2 pl-2 border-brandGray4x sm:w-sixtyPercent sm:max-w-md'>
                 <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M7.33334 13.3333C10.6471 13.3333 13.3333 10.647 13.3333 7.33325C13.3333 4.01954 10.6471 1.33325 7.33334 1.33325C4.01963 1.33325 1.33334 4.01954 1.33334 7.33325C1.33334 10.647 4.01963 13.3333 7.33334 13.3333Z" stroke="#D6D6D6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                     <path opacity="0.4" d="M12.62 13.7931C12.9733 14.8598 13.78 14.9665 14.4 14.0331C14.9666 13.1798 14.5933 12.4798 13.5666 12.4798C12.8066 12.4731 12.38 13.0665 12.62 13.7931Z" stroke="#D6D6D6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
--- src/components/Overview/CountryInsight.jsx
+++ src/components/Overview/CountryInsight.jsx
@@ -10,16 +10,16 @@ const CountryInsight = () => {
 
   return (
     <div className='col-span-2 bg-white rounded-ten p-7'>
-        <div className="flex items-center justify-between gap-5">
+        <div className="flex xs:flex-col items-center justify-between gap-5">
         <h4 className='font-avenirHeavy'>ShoppersBag Country Insight</h4>
-          <label htmlFor="countryInsight">
-            <select name="country-insight" id="countryInsight" className='rounded-fifty pr-2 sm:pr-5 lg:pr-10 focus:ring-inset focus:ring-2 focus:ring-offset-2 focus:ring-brandGreen4x focus:outline-none bg-brandWhite1x text-xs sm:text-sm text-brandGray34x py-2 md:py-2.5 border-0.5 pl-2 border-brandGray34x'>
+          <label htmlFor="countryInsight" className='xs:w-full'>
+            <select name="country-insight" id="countryInsight" className='rounded-fifty pr-2 xs:w-full sm:pr-5 lg:pr-10 focus:ring-inset focus:ring-2 focus:ring-offset-2 focus:ring-brandGreen4x focus:outline-none bg-brandWhite1x text-xs sm:text-sm text-brandGray34x py-2 md:py-2.5 border-0.5 pl-2 border-brandGray34x'>
               <option value="Select country" selected disabled>Select country</option>
             </select>
           </label>
         </div>
 
-        <div className='text-xxs uppercase flex flex-row items-center py-6 gap-4'>
+        <div className='text-xxs uppercase flex xs:flex-col flex-row items-center py-6 gap-4'>
           <div className='flex flex-row items-center gap-2'><div className='w-2 h-2 bg-brandBlue1x rounded-fiftyPercent'></div> Shoppersbag Inactive States  </div>
           <div className='flex flex-row items-center gap-2'><div className='w-2 h-2 bg-brandGreen1x rounded-fiftyPercent'></div> Shoppersbag Active States </div>
         </div>
@@ -31,7 +31,7 @@ const CountryInsight = () => {
           </div>
           <div className='text-xxs pt-8'>
               {CountryInsightData.map((data, index)=>{
-                return <div key={`countryInsight${index}`} id={`countryInsight${index}`} className='border-b-0.5 border-b-brandGray28x last:border-b-0 py-2 flex justify-between items-end gap-5'>
+                return <div key={`countryInsight${index}`} id={`countryInsight${index}`} className='border-b-0.5 border-b-brandGray28x last:border-b-0 py-2 flex xs:gap-2 justify-between items-end gap-5'>
                     <p>{data.insight}</p>
                     <p className='text-brandBlue1x font-avenirBlack w-twentyPercent'>{data.num}</p>
                 </div>
