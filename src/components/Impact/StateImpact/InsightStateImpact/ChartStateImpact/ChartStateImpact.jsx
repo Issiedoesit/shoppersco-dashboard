79cdee4f245e27b4a81a8b36be4c2ebc49775e4b
Isioma Ekwemuka <Isyekwe@gmail.com>
2023-04-12 22:38:44 +0100

    chart height

--- src/components/Impact/StateImpact/InsightStateImpact/ChartStateImpact/ChartStateImpact.jsx
+++ src/components/Impact/StateImpact/InsightStateImpact/ChartStateImpact/ChartStateImpact.jsx
@@ -71,7 +71,7 @@ const ChartStateImpact = () => {
     <div className='border-0.5 border-brandGray42x rounded-ten p-5'>
         <h2 className='pb-10 text-black font-avenirMedium'>Transaction Insight</h2>
         <div className=''>
-            <Bar data={data} options={options} />
+            <Bar data={data} options={options} className="h-254 sm:h-307" />
         </div>
     </div>
   )
