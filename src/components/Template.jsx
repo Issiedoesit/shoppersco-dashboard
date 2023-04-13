92bce53e343e9403af2129438559b3419521c9b0
Isioma Ekwemuka <Isyekwe@gmail.com>
2023-04-13 12:58:11 +0100

    remove template page padding bottom v2

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -3,12 +3,12 @@ import useIsAuthPage from '../customHooks/Stores/useIsAuthPage'
 import Header from './Header/Header'
 
 
-const TemplatePage = ({headerTitle, headerLogo, children, levelTwoRef, paddingBottom }) => {
+const TemplatePage = ({headerTitle, headerLogo, children, levelTwoRef, removePaddingBottom }) => {
 
   const isAuthPage = useIsAuthPage(state => state.isAuthPage)
 
   return (
-    <div className={`${paddingBottom ? 'pb-7' : ''} h-screen overflow-hidden bg-brandGray28x`}>
+    <div className={`${removePaddingBottom ? "" : "pb-7"} h-screen overflow-hidden bg-brandGray28x`}>
       <div ref={levelTwoRef} className={`overflow-y-auto bg-brandGray28x h-full px-5 sm:px-10 ${isAuthPage || 'xl:pl-0 xl:pr-10'}`}>
         <Header page={headerTitle} logo={headerLogo} />
         <div className='pb-28 lg:pb-10 pt-3.5'>
