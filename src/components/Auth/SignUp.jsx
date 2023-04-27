fcc35032724d6a1f220e6049254ecc930373d32a
Isioma Ekwemuka <Isyekwe@gmail.com>
2023-04-27 23:53:39 +0100

    change import method of auth images

--- src/components/Auth/SignUp.jsx
+++ src/components/Auth/SignUp.jsx
@@ -2,6 +2,7 @@ import React, { useLayoutEffect, useRef, useState } from 'react'
 import AuthForm from './AuthForm'
 import useIsAuthPage from '../../customHooks/Stores/useIsAuthPage'
 import $ from 'jquery'
+import signUpImg from './../../../src/assets/images/pngs/signUpImg.png'
 
 
 
@@ -34,7 +35,7 @@ const SignUp = () => {
     <div className='col-span-12 flex py-20 md:py-0 md:grid grid-cols-12 min-h-screen'>
         <AuthForm authHeaderText={'Create your ShoppersBag account'} authPageType={authPageType} />
         <section ref={containerRef} className="absolute md:relative top-0 left-0 login--img--sect col-span-4 lg:col-span-7 xl:col-span-8 h-full">
-            <img src="src/assets/images/pngs/signUpImg.png" alt="hand with oranges" onLoad={handleSkeleton} className='object-cover skeleton min-h-screen h-full' />
+            <img src={signUpImg} alt="hand with oranges" onLoad={handleSkeleton} className='object-cover skeleton min-h-screen h-full' />
             <div className='absolute top-0 left-0 w-full h-full bg-white/20'></div>
             <div id='auto_radius' className='flex justify-center font-avenirLight overflow-hidden w-full absolute top-8 sm:top-5 md:top-auto md:bottom-60 left-0 text-brandGray41x'>
                 <div className='items-center gap-4 mx-auto hidden lg:flex text-center'>
